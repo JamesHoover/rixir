cd3eabc80af56b3595da2f5adaeb4d3a359dddc8
Elijah N20 <ejd.christensen@gmail.com>
2013-12-11 05:36:24 +0000

    rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rixir::Application.config.secret_key_base = 'c9125c84aa1dc51773e0a1305c3ed3039661b70927c5e061cf66647d347bb306042bd87313c6a124717b9108044f979820d9c3666d6685c1b67f7371b9a1a76f'
